9808d3a4130816066e715c22c807b139a6308722
hejl <hjlarry@163.com>
2022-11-28 15:45:53 +0800

    add music player of spotify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./spotify/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./spotify/**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                'white-alpha': {
+                    900: 'rgba(255,255,255, .9)',
+                    800: 'rgba(255,255,255, .8)',
+                    700: 'rgba(255,255,255, .7)',
+                    600: 'rgba(255,255,255, .6)',
+                    400: 'rgba(255,255,255, .4)',
+                    200: 'rgba(255,255,255, .2)',
+                    100: 'rgba(255,255,255, .1)'
+                },
+            }
+        },
+    },
+    plugins: [
+        function ({addUtilities}) {
+            const newUtilities = {
+                '.y-center': {
+                    top: '50%',
+                    transform: 'translateY(-50%)'
+                }
+            }
+            addUtilities(newUtilities)
+        }
+    ],
 }
